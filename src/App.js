df062fc3e41b91fcfe6ed50e58ecda2167122b13
benhexie <benedictgabriel73@gmail.com>
2023-11-03 15:01:11 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Error from './pages/Error';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="*" element={<Error />} />
         </Routes>
       </Router>
